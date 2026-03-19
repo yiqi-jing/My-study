c472d11a3344c8c68fb129143fb1f18cf23d7520
以七-京 <15811025001@163.com>
2026-03-19 10:50:14 +0800

    r语言学习经历

--- R/20260304/test.R
+++
@@ -1,6 +0,0 @@
-# 测试 R 代码
-x = 1:10
-y = x^2
-plot(x, y, main = "VS Code R 测试图")
-print("Hello R in VS Code!")
-print(mean(x))
---
+++ R/20260319（测试题）/test.r
@@ -0,0 +1,23 @@
+# 创建一个数据框grades，包含'student_id','name','math','english','science'五列，
+# 分别表示学生ID、姓名、数学成绩、英语成绩和科学成绩
+
+grades = data.frame(
+    student_id = c(1,2,3,4,5),
+    name       = c('A1','B1','C1','D1','E1'),
+    math       = c(90, 80, 70, 60, 50), #数学
+    english    = c(85, 75, 65, 55, 45), #英语
+    science    = c(95, 85, 75, 65, 55)  #科学
+)
+
+print(grades)
+
+# 在数据框中新增total_score，计算每个学生的总分
+grades$total_score = grades$math + grades$english + grades$science
+print(grades)
+
+# 筛选出数学成绩 ≥ 90 或总分 ≥ 260 ，输出姓名和数学成绩和总分
+filtered_grades = grades[grades$math >= 90 | grades$total_score >= 260, c('name', 'math', 'total_score')]
+print(filtered_grades)
+# 按总分的高低对数据框进行排序，并保留前3名学生的所有消息
+top_students = grades[order(-grades$total_score), ][1:3, ]
+print(top_students)
